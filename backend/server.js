304730c1aebd3286fb284a7b68a31fe514776dd7
EzgiLenaS <ezgilena00@gmail.com>
2025-02-06 18:28:11 +0300

    Initial commit. Database connected to project.

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend bitches!!");
+})
+app.listen(3000, () => {
+    connectDB();
+    console.log("Server is running on port 3000");
+});
