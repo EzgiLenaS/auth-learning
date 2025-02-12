97f253ac3e06141a626d82ad0f8ee84f4ad22a96
EzgiLenaS <ezgilena00@gmail.com>
2025-02-12 23:06:56 +0300

    Login endpoint is added. Next Forgot password endpoint.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,9 +87,41 @@ export const verifyEmail = async (req, res) => {
     }
 }
 export const login = async (req, res) => {
-    res.send("Login")
+    const { email, password } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+		const isPasswordValid = await bcryptjs.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(res, user._id);
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		console.log("Error in login ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
 };
 
 export const logout = async (req, res) => {
-    res.send("Logout")
+    // We will clear out the cookies
+    // So we will now that user is unauthanticated
+    res.clearCookie("token");
+    // We wrote "token" because in generateTokenAndSetCookie
+    // We used "token" as a key
+    res.status(200).json({success: true, message: "Logged out successfully"});
 };
