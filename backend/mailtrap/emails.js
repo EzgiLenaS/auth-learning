8f74cb5384e068ab4434de928d6077b053c4717b
EzgiLenaS <ezgilena00@gmail.com>
2025-02-20 13:15:37 +0300

    Forgot password and Reset password endpoints are done. Next Chech Auth Endpoint

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,4 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.config.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -37,5 +37,44 @@ export const sendWelcomeEmail = async (email, name) => {
     } catch (error) {
         console.error(`Error sending welcome email`, error);
         throw new Error(`Error sending welcome email: ${error}`);
+    }
+}
+
+export const sendPasswordResetEmail = async (email, resetPasswordLink) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetPasswordLink),
+            category: "Password Reset",
+        });
+
+        console.log("Email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending reset email`, error);
+
+        throw new Error(`Error sending reset email: ${error}`);
+    }
+}
+
+export const sendResetSuccessEmail = async (email) => {
+    const recipient = [{ email }];
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Password Reset Successful",
+            html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+            category: "Password Reset",
+        });
+
+        console.log("Email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending success reset password email`, error);
+
+        throw new Error(`Error sending success reset password email: ${error}`);
     }
 }
